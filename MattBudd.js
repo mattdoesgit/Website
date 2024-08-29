b339da6747b683408c2a7caae999d75fffecb483
mattdoesgit <mab269@cam.ac.uk>
2024-08-29 20:18:01 +0100

    Update MattBudd.js

---
+++ MattBudd.js
@@ -0,0 +1,19 @@
+var coll = document.getElementsByClassName("collapsible");
+
+// Add event listeners to each button
+for (var i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function() {
+        // Toggle between adding and removing the "active" class
+        this.classList.toggle("active");
+        
+        // Get the content element associated with the button
+        var content = this.nextElementSibling;
+        
+        // Check if the content is already open or not
+        if (content.style.display === "block") {
+            content.style.display = "none";
+        } else {
+            content.style.display = "block";
+        }
+    });
+}
