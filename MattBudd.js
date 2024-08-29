28b6c620ef82b948d601f8bc6230ac684a4028ef
mattdoesgit <mab269@cam.ac.uk>
2024-08-29 23:26:59 +0100

    Update MattBudd.js

--- MattBudd.js
+++ MattBudd.js
@@ -1,4 +1,4 @@
-var coll = document.getElementsByClassName("collapsible");
+var coll = document.getElementsByClassName("content");
 
 // Add event listeners to each button
 for (var i = 0; i < coll.length; i++) {
